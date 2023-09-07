6d6d414195018273bf577c7d5b6c060ad7299b70
kymullin <kyle.mullins@ttu.edu>
2023-09-07 16:20:04 -0500

    Separated 'testbench' module into separate file.

--- miniproject/motorcontrol.v
+++ miniproject/motorcontrol.v
@@ -4,16 +4,3 @@ module motorcontrol(out, PWM, SW);
     assign out[0] = (~SW & PWM);
     assign out[1] = (SW & PWM);
 endmodule
-
-module testbench;
-    reg PWM, SW;
-    wire [1:0] out;
-    motorcontrol UUT(out, PWM, SW);
-    initial begin
-        $monitor("PWM=%d SW=%d out=%b", PWM, SW, out);
-        PWM = 1'b0;     SW = 1'b0;  #1;
-        PWM = 1'b0;     SW = 1'b1;  #1;
-        PWM = 1'b1;     SW = 1'b0;  #1;
-        PWM = 1'b1;     SW = 1'b1;  #1;
-    end
-endmodule
